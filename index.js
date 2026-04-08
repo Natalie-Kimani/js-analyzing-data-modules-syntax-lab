8ac245476ff83f32bc1ce6d19bc323254a793e00
Natalie Kimani <anitakimani07@gmail.com>
2026-04-08 20:15:28 +0300

    Modules

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  // loop through arrays
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...args[i]
+    ];
+  }
+
+  // add merge date
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
